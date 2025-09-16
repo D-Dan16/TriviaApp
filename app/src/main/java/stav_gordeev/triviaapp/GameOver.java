24485c985a3305f0f3c0362af0001594da970fbf
stav <stav.gr8@gmail.com>
2025-09-16 10:01:48 +0300

    Added the Game Over activity and its code

--- app/src/main/java/stav_gordeev/triviaapp/GameOver.java
+++ app/src/main/java/stav_gordeev/triviaapp/GameOver.java
@@ -1,6 +1,8 @@
 package stav_gordeev.triviaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class GameOver extends AppCompatActivity {
 
+    LottieAnimationView lavGameOver;
+    TextView tvUserScore,tvPointsGO,tvAllScores;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,23 @@ public class GameOver extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        init();
+        Intent intent=getIntent();
+        int points = intent.getIntExtra("points",0);
+        String email=intent.getStringExtra("email");
+
+        tvPointsGO.setText(Integer.toString(points));
+        tvUserScore.setText(email);
+
     }
+
+    public void init()
+    {
+        tvPointsGO=findViewById(R.id.tvPointsGO);
+        tvUserScore=findViewById(R.id.tvUserScore);
+        tvAllScores=findViewById(R.id.tvAllScores);
+    }
+
+
 }
