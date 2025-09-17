26c81aa1075e8ea9b1986dcd040e9d5a0aab11e3
Stav <stav.gr8@gmail.com>
2025-09-17 14:39:17 +0300

    Added an unused Settings Activity lol Also added an Instructions Activity that has no true purpose but to use from a different place a .txt file!

--- app/src/main/java/stav_gordeev/triviaapp/ExpIntro.java
+++ app/src/main/java/stav_gordeev/triviaapp/ExpIntro.java
@@ -1,14 +1,27 @@
 package stav_gordeev.triviaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class ExpIntro extends AppCompatActivity {
+
+    private TextView tvInstructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,40 @@ public class ExpIntro extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        init();
+        String all=readIntroductionsFromFile();
+        tvInstructions.setText(all);
+    }
+
+    public void init()
+    {
+        tvInstructions=findViewById(R.id.tvInstructions);
+    }
+
+
+
+    public String readIntroductionsFromFile()
+    {
+        InputStream is;
+        InputStreamReader isr;
+        BufferedReader br;
+        is=getResources().openRawResource(R.raw.instructions);
+        isr=new InputStreamReader(is);
+        br=new BufferedReader(isr);
+        String all="";
+        String st;
+        try {
+            st=br.readLine();
+            while (st!=null) {
+                all+=st+"\n";
+                st=br.readLine();
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            Toast.makeText(this,"could not open", Toast.LENGTH_SHORT).show();
+        }
+        return all;
     }
 }
